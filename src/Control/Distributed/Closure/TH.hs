f7e552481da303190bc5adf9e1cb863e62298958
Mathieu Boespflug <m@tweag.io>
2015-05-25 15:35:36 +0200

    document 'deriveClosureDict'.

--- src/Control/Distributed/Closure/TH.hs
+++ src/Control/Distributed/Closure/TH.hs
@@ -10,6 +10,20 @@ import           Control.Distributed.Closure.Internal
 import           GHC.StaticPtr
 import qualified Language.Haskell.TH as TH
 
+-- | Derive a 'ClosureDict' instance for the given instance context and instance
+-- head. Examples:
+--
+-- > deriveClosureDict [t| Eq Int |]
+-- > deriveClosureDict [t| Eq Bool |]
+-- > deriveClosureDict [t| (Eq a, Eq b) => Eq (a, b) |]
+-- > deriveClosureDict [t| Typeable a => Serializable (Proxy a) |]
+--
+-- which each derive the following instances,
+--
+-- > instance ClosureDict (Eq Int) where closureDict = closure (static Dict)
+-- > instance ClosureDict (Eq Bool) where closureDict = closure (static Dict)
+-- > instance (ClosureDict (Eq a), ClosureDict (Eq b)) => ClosureDict (Eq (a, b)) where ...
+-- > instance ClosureDict (Typeable a) => ClosureDict (Serializable (Proxy a)) where ...
 deriveClosureDict :: TH.TypeQ -> TH.DecsQ
 deriveClosureDict = (>>= go)
   where
