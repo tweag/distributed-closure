4e24a42367f18aa79e5806b14af195f9356c749a
Mathieu Boespflug <m@tweag.io>
2015-05-25 13:41:10 +0200

    Derive ClosureDict instances for 0-tuples to 8-tuples.

--- src/Control/Distributed/Closure.hs
+++ src/Control/Distributed/Closure.hs
@@ -24,3 +24,17 @@ module Control.Distributed.Closure
 import           Control.Distributed.Closure.TH
 import           Control.Distributed.Closure.Internal
 import           Data.Constraint (Dict(..))
+
+deriveClosureDict [t| Serializable () |]
+deriveClosureDict
+  [t| forall a b. (Serializable a, Serializable b) => Serializable (a, b) |]
+deriveClosureDict
+  [t| forall a b c. (Serializable a, Serializable b, Serializable c) => Serializable (a, b, c) |]
+deriveClosureDict
+  [t| forall a b c d. (Serializable a, Serializable b, Serializable c, Serializable d) => Serializable (a, b, c, d) |]
+deriveClosureDict
+  [t| forall a b c d e. (Serializable a, Serializable b, Serializable c, Serializable d, Serializable e) => Serializable (a, b, c, d, e) |]
+deriveClosureDict
+  [t| forall a b c d e f. (Serializable a, Serializable b, Serializable c, Serializable d, Serializable e, Serializable f) => Serializable (a, b, c, d, e, f) |]
+deriveClosureDict
+  [t| forall a b c d e f g. (Serializable a, Serializable b, Serializable c, Serializable d, Serializable e, Serializable f, Serializable g) => Serializable (a, b, c, d, e, f, g) |]
