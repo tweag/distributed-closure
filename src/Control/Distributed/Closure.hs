8ff34c49a5f7a224c631d283c0002f27b5fafb1b
Mathieu Boespflug <m@tweag.io>
2016-04-03 10:53:41 +0200

    typo.

--- src/Control/Distributed/Closure.hs
+++ src/Control/Distributed/Closure.hs
@@ -23,7 +23,7 @@ import Data.Constraint (Dict(..))
 --
 -- A 'Dict' reifies a constraint in the form of a first class value. The 'Dict'
 -- type is not serializable: how do you serialize the constraint that values of
--- this type carry? However, for any constraint @c@, a value of type @'Closure'
+-- this type carry? Whereas, for any constraint @c@, a value of type @'Closure'
 -- ('Dict' c)@ /can/ be serialized and sent over the wire, just like any
 -- 'Closure'. A /serializable dictionary/ for some constraint @c@ is a value of
 -- type @'Closure' ('Dict' c)@.
